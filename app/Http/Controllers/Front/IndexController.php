09f91da6f8ee888b4cd82ffd790d8300fd05b595
jhraj963 <jhraj963@gmail.com>
2025-05-04 15:10:26 +0600

    Deals of the Week Option

--- app/Http/Controllers/Front/IndexController.php
+++ app/Http/Controllers/Front/IndexController.php
@@ -20,11 +20,12 @@ class IndexController extends Controller
         $popular_product=Product::where('status', 1)->orderBy('product_views','DESC')->limit(16)->get();
         $random_product=Product::where('status', 1)->inRandomOrder()->limit(18)->get();
         $trendy_product=Product::where('status', 1)->where('trendy', 1)->orderBy('product_views','DESC')->limit(10)->get();
+        $todaydeal=Product::where('status', 1)->where('today_deal', 1)->orderBy('id','DESC')->limit(8)->get();
 
         //For Home Category
         $home_category = DB::table('categories')->where('home_page',1)->orderBy('category_name','ASC')->get();
 
-        return view('frontend.index', compact('category', 'bannerproduct', 'featured', 'popular_product', 'trendy_product','home_category', 'brand', 'random_product'));
+        return view('frontend.index', compact('category', 'bannerproduct', 'featured', 'popular_product', 'trendy_product','home_category', 'brand', 'random_product', 'todaydeal'));
     }
 
     // show single product
